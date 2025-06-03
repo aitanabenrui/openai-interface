b4311ce7060892ec701189d69e80b5379570adba
aitanabenrui <aitanabenrui2001@gmail.com>
2025-06-03 18:56:33 +0200

    feat: font family

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css'
 import { Aside } from './components/aside/aside'
+import { Button } from './components/button/Button'
 
 function App() {
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <div className='aside_container'>
       <Aside/>
+      <Button label='Add payment details'></Button>
     </div>
   )
 }
