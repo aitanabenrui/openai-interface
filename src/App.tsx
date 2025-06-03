96d6765e3811b488d8c868f8e8cd5f6b9e595ae8
aitanabenrui <aitanabenrui2001@gmail.com>
2025-06-03 19:40:32 +0200

    style: font

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import './App.css'
 import { Aside } from './components/aside/aside'
-import { Button } from './components/button/Button'
-
 function App() {
 
 
   return (
     <div className='aside_container'>
       <Aside/>
-      <Button label='Add payment details'></Button>
     </div>
   )
 }
