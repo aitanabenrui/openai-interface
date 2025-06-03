2582ad70901a6262d031f6a2feaec6fb43b3ccd9
aitanabenrui <aitanabenrui2001@gmail.com>
2025-06-03 17:41:35 +0200

    style: margin buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import './App.css'
-import { Button } from './components/button/button'
+import { Button } from './components/button/Button'
 
 function App() {
 
   return (
     <>
-   <Button label="Add payment details" variant="green" onClick={() => alert('¡Compra!')} />
-<Button label="View usage" variant="gray" />
+      <Button label="Add payment details" variant="green" onClick={() => alert('¡Compra!')} />
+      <Button label="View usage" variant="gray" />
     </>
   )
 }
